3e1b96980e5416d4210a871157ce42c1f8ddb6b2
Pablo Fernandez <pfernand@linkedin.com>
2010-09-02 14:53:23 -0700

    Modified nus tests to reflect the actual problem

--- public/js/tests/nus/handle_errors.js
+++ public/js/tests/nus/handle_errors.js
@@ -1,7 +1,7 @@
-new IN.Test.TestCase('Should only work for the actual user', function(){
+new IN.Test.TestCase('Should fail for any user but the owner', function(){
   try
   {
-    IN.API.NetworkUpdates("not-me","other-id","more ids").ids('asdf')
+    IN.API.NetworkUpdates("not-me")
     .all(function(data){
       this.fail("should not call all()");
     }, this);  
@@ -10,7 +10,20 @@ new IN.Test.TestCase('Should only work for the actual user', function(){
     this.assertTrue(true, "expected exception: " + e);
     this.finish();
   }
-  
+}, {'category':'network-updates'});
+
+new IN.Test.TestCase('Should fail for any user but the owner (multiple ids)', function(){
+  try
+  {
+    IN.API.NetworkUpdates("not-me", "other-id")
+    .all(function(data){
+      this.fail("should not call all()");
+    }, this);  
+  }catch(e)
+  {
+    this.assertTrue(true, "expected exception: " + e);
+    this.finish();
+  }
 }, {'category':'network-updates'});
 
 new IN.Test.TestCase('Should fail for unexistent fields', function(){
