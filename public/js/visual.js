1c0beddd10b0fad9d5a38d6dfa6a4a43994da16c
Pablo Fernandez <fernandezpablo85@gmail.com>
2010-09-09 11:20:13 -0300

    Now with test results

--- public/js/visual.js
+++ public/js/visual.js
@@ -12,9 +12,17 @@ IN.Test.Controls.panelEntry = function(suite)
 
 IN.Test.Controls.suiteResults = function(result)
 {
-  var div = $("<div>").addClass('entry');
+  var suite = result.testSuite;
   var clazz = (result.results.failed > 0) ? "fail" : "pass";
-  div.append($('<h4>').text(result.testSuite.name).addClass(clazz));
-  console.log(result);
+  var div = $("<div>").addClass('entry').addClass(clazz);
+  div.append($('<h4>').text(suite.name));
+  var ul = $('<ul/>')
+  for(var i in suite.items)
+  {
+    var key = suite.items[i].name;
+    var testResult = result.results[key];
+    ul.append($('<li>').text(testResult.name));
+  }
+  div.append(ul);
   return div;
 }
