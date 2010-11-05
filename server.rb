24a80fea9c9985a6025ca46c399c5946fd0aed24
Pablo Fernandez <fernandezpablo85@gmail.com>
2010-11-05 14:28:30 -0300

    Added helper method

--- server.rb
+++ server.rb
@@ -6,3 +6,13 @@ require "sinatra"
 get '/' do
   erb :index
 end
+
+helpers do
+  
+  def connect_tag(params)
+    """ <script type='text/javascript' src='http://#{params[:server]}/uas/in.js'>
+          api_key: #{params[:key]}
+          onLoad: IN.Test.onLogin
+        </script>"""
+  end
+end
